0197b16179a25b8e02e8cf67783029142dad1b46
Florence Martin <florence.martin469@gmail.com>
2023-01-19 12:23:01 +0100

    feat: changes files to deploy

--- frontend/src/service/index.js
+++ frontend/src/service/index.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export function getPosts() {
   return new Promise((resolve) => {
     axios
-      .get("http://localhost:4000/posts")
+      .get("/posts")
       .then((res) => res.status === 200 && res.data)
       .then(resolve)
       .catch(console.error);
